c16b62232b9a7cc046bd35d410a63e45856e6e1a
Piotr-Augustynski <augustynskipiotr@gmail.com>
2018-07-05 18:48:18 +0200

    order as a state machine

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,9 +1,30 @@
 class Order < ApplicationRecord
+  include Statesman::Adapters::ActiveRecordQueries
   belongs_to :shipping_type
   has_many :line_items
+  has_many :transitions, class_name: 'OrderTransition', autosave: false
   has_one :address
 
+  delegate :can_transition_to?, :transition_to!, :transition_to, :current_state, to: :state_machine
+
+  def state_machine
+    @state_machine ||= OrderStateMachine.new(self, transition_class: OrderTransition,
+                                              association_name: :transitions)
+  end
+
   def full_cost
-    line_items.map { |e| e.full_price }.sum +shipping_cost
+    line_items.map { |e| e.full_price }.sum + shipping_cost
+  end
+
+  def self.transition_state
+    OrderTransition
+  end
+
+  def self.initial_class
+    OrderStateMachine.initial_state
+  end
+
+  def self.transition_name
+    :transitions
   end
 end
